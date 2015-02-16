1d6aa5a44776f8344731bb06ead958b6bc958753
Luke Ring <luke.ring@vu.edu.au>
2015-02-17 09:57:45 +1100

    fix bug, works from first load

--- chrome/index.js
+++
@@ -1,8 +0,0 @@
-chrome.contextMenus.create({
-	title:'Remove This',
-	contexts:["all"],
-	onclick: function(info, tab) {
-		chrome.tabs.executeScript(null, {file: "content.js"});
-        chrome.tabs.sendMessage(tab.id, 'deleteCurrentItem');
-    }
-});
